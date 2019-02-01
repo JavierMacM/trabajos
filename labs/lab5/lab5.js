cc9d8516d8941aa794815d5ff0a3bfc5ac573303
JavierMacM <43215688+JavierMacM@users.noreply.github.com>
2019-01-31 19:08:21 -0600

    lab 5 en desarrollo
    falta agregar js

---
+++ labs/lab5/lab5.js
@@ -0,0 +1,23 @@
+//funcion para el ejercicio 1 validar contrasena
+function validatePassword(pass1, pass2){
+  var message;
+
+  if(pass1 === pas2){
+    message = "Acceso obtenido";
+  }else
+  if(pass1==="" || pass2===""){
+    message = "Favor de llenar ambos campos";
+  }else
+  if(pass1 != pass2){
+    message="Las contrasenas no coinciden";
+  }
+}
+
+function payProducts(){
+  
+}
+
+//funcion para el ejercicio 2 compra de productos
+
+
+//funcion para el ejercicio 3 genrar historia
