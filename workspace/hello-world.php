21c920f7fd7566866272694d13a9c689dc977dab
JavierMacM <43215688+JavierMacM@users.noreply.github.com>
2019-05-17 11:58:24 -0500

    notrash

--- workspace/hello-world.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("views/_header.html");
-
-include("views/_tablaIncidentes.html");
-
-include("views/_formaRegistroIncidente.html");
-
-include("views/_footer.html");
-?>
--- workspace/pages/jurassicpark.php
+++
@@ -1,4 +0,0 @@
-<?php include("../views/_header.html")?>
-<?php include("../views/_formaRegistroIncidente.php")?>
-<?php include("../views/_tablaIncidentes.php")?>
-<?php include("../views/_footer.html")?>
